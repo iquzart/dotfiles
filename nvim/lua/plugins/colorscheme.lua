73275f7a5d023558d806eda6ae17b3123ab185ce
Muhammed Iqbal <muhammed.iqbal@zand.ae>
2023-11-28 10:31:42 +0400

    nvim setup

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = true,
+  -- opts = { style = "night" },
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
+
---
+++ nvim/lua/plugins/extras.lua
@@ -0,0 +1,38 @@
+return {
+
+    -- Coding
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+
+    -- Lang
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.python-semshi" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+
+    -- Dap
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
+
+    -- Editor
+    { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+    { import = "lazyvim.plugins.extras.editor.leap" },
+
+    -- Formatting
+    { import = "lazyvim.plugins.extras.formatting.black" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+
+    -- Lint
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+
+    -- Devops
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
+
+    -- Utils
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.util.dot" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
+
+
+}
