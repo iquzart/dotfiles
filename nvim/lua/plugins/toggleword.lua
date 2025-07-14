f5b35043a88f24a6fc43daaa0f3f7d869ce8a582
Muhammed Iqbal <iquzart@hotmail.com>
2025-07-14 11:39:25 +0400

    [nvim] enhancement toggleword plugin

--- nvim/lua/plugins/toggleword.lua
+++ nvim/lua/plugins/toggleword.lua
@@ -1,31 +1,8 @@
-local M = {}
-
-M.toggles = {
-  ["true"] = "false",
-  ["false"] = "true",
-  ["on"] = "off",
-  ["off"] = "on",
-  ["enabled"] = "disabled",
-  ["disabled"] = "enabled",
-  ["yes"] = "no",
-  ["no"] = "yes",
-  ["up"] = "down",
-  ["down"] = "up",
+return {
+  "iquzart/toggleword.nvim",
+  config = function()
+    require("toggleword").setup({
+      key = "<leader>tt", -- optional, defaults to <leader>tt
+    })
+  end,
 }
-
-function M.toggle_word()
-  local word = vim.fn.expand("<cword>")
-  local replacement = M.toggles[word]
-  if replacement then
-    vim.cmd("normal! ciw" .. replacement)
-  else
-    vim.notify("No toggle match for: " .. word, vim.log.levels.WARN)
-  end
-end
-
--- Keymap defined here
-vim.keymap.set("n", "<leader>tt", function()
-  M.toggle_word()
-end, { desc = "Toggle word under cursor" })
-
-return M
