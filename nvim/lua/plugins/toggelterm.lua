94211b4b4277b809d858bbac8cddb0d85350b8bb
Muhammed Iqbal <iquzart@hotmail.com>
2025-08-13 18:40:56 +0400

    updated toogelterm and switch to ghostty

--- nvim/lua/plugins/toggelterm.lua
+++ nvim/lua/plugins/toggelterm.lua
@@ -41,20 +41,21 @@ return {
       close_on_exit = true, -- close the terminal window when the process exits
       -- shell = vim.o.shell, -- change the default shell
       -- This field is only relevant if direction is set to 'float'
-      -- float_opts = {
-      --   -- The border key is *almost* the same as 'nvim_open_win'
-      --   -- see :h nvim_open_win for details on borders however
-      --   -- the 'curved' border is a custom border type
-      --   -- not natively supported but implemented in this plugin.
-      --   border = 'single' or 'double' or 'shadow' or 'curved',
-      --   width = <value>,
-      --   height = <value>,
-      --   winblend = 3,
-      --   highlights = {
-      --     border = "Normal",
-      --     background = "Normal",
-      --   }
-      -- }
+      float_opts = {
+        -- The border key is *almost* the same as 'nvim_open_win'
+        -- see :h nvim_open_win for details on borders however
+        -- the 'curved' border is a custom border type
+        -- not natively supported but implemented in this plugin.
+        -- border = 'single' or 'double' or 'shadow' or 'curved',
+        border = "curved",
+        -- width = <value>,
+        -- height = <value>,
+        -- winblend = 3,
+        -- highlights = {
+        --   border = "Normal",
+        --   background = "Normal",
+        -- }
+      },
     },
   },
 }
