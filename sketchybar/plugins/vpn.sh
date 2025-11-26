fe2275c90d323d3c8aaf7b40807553cf5d1ffdc1
Muhammed Iqbal <iquzart@hotmail.com>
2025-11-26 16:10:47 +0400

    fix interface

--- sketchybar/plugins/vpn.sh
+++ sketchybar/plugins/vpn.sh
@@ -4,18 +4,18 @@ source "$CONFIG_DIR/colors.sh"
 source "$CONFIG_DIR/icons.sh"
 
 IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
-IS_VPN=$(ifconfig | grep -m1 'ppp0' | awk '{ print $1 }')
+IS_VPN=$(ifconfig | grep -m1 'utun5' | awk '{ print $1 }')
 
 if [[ $IS_VPN != "" ]]; then
-	COLOR=$GREEN
-	ICON=$VPN_CONNECTED
-	LABEL="VPN"
+  COLOR=$GREEN
+  ICON=$VPN_CONNECTED
+  LABEL="VPN"
 else
-	COLOR=$GREY
-	ICON=$VPN_DISCONNECTED
-	LABEL="VPN"
+  COLOR=$GREY
+  ICON=$VPN_DISCONNECTED
+  LABEL="VPN"
 fi
 
 sketchybar --set $NAME background.color=$COLOR \
-	icon=$ICON \
-	label="$LABEL"
+  icon=$ICON \
+  label="$LABEL"
