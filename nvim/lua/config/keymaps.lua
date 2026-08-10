348e3a123cd7115899c57bf37a0dc05d4d075ca4
Muhammed Iqbal <iquzart@hotmail.com>
2026-08-10 12:02:22 +0400

    added dc shortcut to remove blanklines and comments

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>dc", function()
+  local comment = vim.bo.commentstring:match("^(.-)%%s")
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local kept = {}
+
+  for _, line in ipairs(lines) do
+    local is_blank = line:match("^%s*$")
+    local is_comment = comment and comment ~= "" and line:match("^%s*" .. vim.pesc(comment))
+
+    if not is_blank and not is_comment then
+      table.insert(kept, line)
+    end
+  end
+
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, kept)
+end, { desc = "Delete comment and blank lines" })
